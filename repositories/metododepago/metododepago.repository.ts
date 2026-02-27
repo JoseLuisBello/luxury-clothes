de9d8b9b3ecc7105c02623a6584a5408ed33ec04
Luisdiazantonio <luisdiaz09446@gmail.com>
2026-02-27 16:20:24 -0600

    [ADD] agregar nuevos metodo

--- repositories/metododepago/metododepago.repository.ts
+++ repositories/metododepago/metododepago.repository.ts
@@ -27,6 +27,15 @@ export class MetodoDePagoRepository {
         return rows;
     }
 
+    static async obtenermetodo(id_cliente: number, id_metodo: number) {
+        const { rows } = await pool.query(
+            `SELECT * FROM "MetodoDePago"
+                WHERE "id_cliente" = $1 AND "id" = $2`,
+            [id_cliente, id_metodo]
+        );
+        return rows[0];
+    }
+
     // Agregar nuevo metodo
     // 1	Tarjeta de crédito              requiere numero de tarjeta, fecha de vencimiento y nombre titular
     // 2	Tarjeta de débito	            requiere numero de tarjeta, fecha de vencimiento y nombre titular
@@ -55,6 +64,32 @@ export class MetodoDePagoRepository {
         return rows[0];
     }
 
+    static async modificarMetodo(
+        id_cliente: number,
+        id_metodo: number,
+        numero_cuenta: string | null,
+        nombre_titular: string | null,
+        fecha_vencimiento: string | null,
+        banco: string | null,
+        correo: string | null,
+        proveedor: string | null
+    ) {
+        // solo permitir cambios en el mismo metodo, si quiere un nuevo tipo debe agregar un nuevo metodo
+        const { rows } = await pool.query(
+            `UPDATE "MetodoDePago" SET 
+            "numero_cuenta" = $1, "nombre_titular" = $2, "fecha_vencimiento" = $3, "banco" = $4, "correo" = $5, "proveedor" = $6            
+            WHERE "id_cliente" = $7 AND "id" = $8
+             RETURNING *`,
+            [numero_cuenta, nombre_titular, fecha_vencimiento, banco, correo, proveedor, id_cliente, id_metodo]
+        );
+        // Ejemplo de Update en base
+        // UPDATE "MetodoDePago" SET 
+        // "numero_cuenta" = null, "nombre_titular" = null , "fecha_vencimiento" = null , "banco" = null , "correo" = null , "proveedor" = 'Apple Pay'
+        // WHERE "id_cliente" = 1 AND "id" = 7
+        // RETURNING *;
+        return rows[0] ?? null;
+    }
+
     static async eliminarMetodo(
         id_cliente: number,
         id_metodo: number) {
@@ -65,6 +100,6 @@ export class MetodoDePagoRepository {
              RETURNING *`,
             [id_cliente, id_metodo]
         );
-        return rows[0]?? null
+        return rows[0] ?? null
     }
 }
