b1006aba40afd9d72f018cd39918ac3843abb719
adrien04hz <adrien.ahs@gmail.com>
2026-03-16 17:02:29 -0600

    [ADD] Fetch general para proyecto

---
+++ lib/api.ts
@@ -0,0 +1,17 @@
+const API_URL = process.env.API_URL;
+
+export async function apiFetch(endpoint: string, options?: RequestInit) {
+  const res = await fetch(`${API_URL}${endpoint}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...options?.headers
+    }
+  })
+
+  if (!res.ok) {
+    throw new Error("API Error")
+  }
+
+  return res.json()
+}
