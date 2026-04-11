9a4707ae40fc8d33767178aca9d0dd03ad2a5cc3
adrien04hz <adrien.ahs@gmail.com>
2026-04-10 18:29:50 -0600

    [ADD] Breadcrumb estático

--- app/productos/page.tsx
+++ app/productos/page.tsx
@@ -1,25 +1,32 @@
-// # pagina de inicio principal, listado de todos los productos haciendo uso de filtrado
 import { getCatalogo } from "@/client/producto.client";
+import Link from "next/dist/client/link";
 import Image from "next/image";
-export default async function Home() {
+export default async function Productos() {
 
-  const response = await getCatalogo({id_categoria: 1});
-  const productos = response.productos; // por como regresas los datos en tu api
+  // const response = await getCatalogo({id_categoria: 1});
+  // const productos = response.productos; // por como regresas los datos en tu api
 
   return (
-    <>
+    // div principal
+    <div className="flex flex-col justify-center items-start">
+      {/* breadcrumb */}
+      <div className="flex items-center justify-start border space-x-2 w-fit">
+        <div className="hover:underline w-fit">
+          <Link
+            href={"#"}
+          >Ropa</Link>
+        </div>
+        <span>/</span>
+        <div className="hover:underline w-fit">
+          <Link href={"#"}>Hombre</Link>
+        </div>
+        <span>/</span>
+        <div className="hover:underline w-fit">
+          <Link href={"#"}>Chamarra</Link>
+        </div>
+      </div>
 
-      <h1>Soy solo el contenido</h1>
-      {
-        productos?.map((producto) => (
-          <div key={producto.id}>
-            <h2>{producto.nombre}</h2>
-            <p>{producto.descripcion}</p>
-            <Image src={producto.imagen_url || "/default-image.jpg"} alt={producto.nombre} width={200} height={200}/>
-          </div>
-        ))
-      }
-      
-    </>
-  )
+      {/* titulo y apartado de filtro */}
+    </div>
+  );
 }
