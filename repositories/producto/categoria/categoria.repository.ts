73a4781dc363f436caf4984db4ab302f63b3eabd
adrien04hz <adrien.ahs@gmail.com>
2026-04-15 04:51:45 -0600

    [ADD] Repositorio para obtener categorias disponibles

--- repositories/producto/categoria/categoria.repository.ts
+++ repositories/producto/categoria/categoria.repository.ts
@@ -42,4 +42,41 @@ export class CategoriaRepository {
         );
         return rows;
     }
+
+    /**
+     * Obtener todas las categorias con subcategorias
+     * disponibles en la base de datos
+     * @returns Lista de categorías con sus subcategorías
+     */
+    static async getTodasLasCategorias() {
+        const { rows } = await pool.query(
+            `
+            SELECT 
+            c.id AS id_categoria,
+            c.nombre AS categoria,
+            json_agg(
+                jsonb_build_object(
+                'id', s.id,
+                'nombre', s.nombre,
+                'total', s.total_productos
+                )
+            ) AS subcategorias
+            FROM "Categoria" c
+            JOIN (
+            SELECT 
+                s.id,
+                s.nombre,
+                s.id_categoria,
+                COUNT(p.id) as total_productos
+            FROM "Subcategoria" s
+            JOIN "Producto" p ON p.id_subcategoria = s.id
+
+            GROUP BY s.id, s.nombre, s.id_categoria
+            ) s ON s.id_categoria = c.id
+            GROUP BY c.id, c.nombre
+            ORDER BY c.id;
+            `
+        );
+        return rows;
+    }   
 }
