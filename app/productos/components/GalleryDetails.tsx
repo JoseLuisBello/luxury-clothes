0936b7b4b367edd1f30a4c8c21b6c393eec55502
adrien04hz <adrien.ahs@gmail.com>
2026-04-15 16:42:58 -0600

    [FIX] Llamada doble de detalles de producto

--- app/productos/components/GalleryDetails.tsx
+++ app/productos/components/GalleryDetails.tsx
@@ -15,14 +15,14 @@ export default function GalleryDetails({
             {/* Imagenes de costado */}
             <div className="flex flex-col justify-start items-center space-y-3">
                 {data.imagenes && data.imagenes.map((imgUrl, index) => (
-                    <div className="w-17.5 h-17.5 overflow-hidden rounded-[10px]" key={index}>
+                    <div className="w-17.5 h-17.5 overflow-hidden rounded-[10px] flex justify-center items-center" key={index}>
                         <Image
                             key={index}
                             src={imgUrl}
                             alt={`${data.nombre} - Imagen ${index + 1}`}
                             width={70}
-                            height={70}
-                            className="mb-4 cursor-pointer border rounded hover:opacity-80"
+                            height={100}
+                            className="mb-4 cursor-pointer rounded-[10px] hover:opacity-80"
                             onClick={() => setURL(imgUrl)}
                             onMouseEnter={() => setURL(imgUrl)}
                             objectFit="cover"
@@ -34,11 +34,11 @@ export default function GalleryDetails({
             {/* Imagen principal */}
             <div className="w-133.75 h-167.25">
                 <Image
-                    src={url ? url.toString() : data.imagen_url || "/placeholder.png"}
+                    src={url ? url.toString() : data.imagenes?.[0] || "/placeholder.png"}
                     alt={data.nombre}
                     width={400}
                     height={400}
-                    className="object-cover w-full h-full rounded-[10px]"
+                    className="object-cover w-full h-full rounded-[10px] border border-gray-200"
                     loading="eager"
                 />
             </div>
