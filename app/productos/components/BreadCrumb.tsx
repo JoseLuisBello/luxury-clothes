06219da800a0db37ec023e17af351f52a3b6ff14
adrien04hz <adrien.ahs@gmail.com>
2026-04-15 17:31:58 -0600

    [ADD] Breadcrumb generator

--- app/productos/components/BreadCrumb.tsx
+++ app/productos/components/BreadCrumb.tsx
@@ -1,21 +1,44 @@
 import Link from "next/link";
+import { breadCrumbs } from "../utils/producto";
 
-export default function BreadCrumb() {
+export default function BreadCrumb({ categoria, subcategoria, genero }: { categoria: number; subcategoria?: number; genero?: number }) {
+
+    const crumbs = breadCrumbs({
+        id_categoria: categoria,
+        id_subcategoria: subcategoria,
+        id_genero: genero
+    });
+
     return (
         <div className="flex items-center justify-start space-x-2 w-fit mt-3 mb-10 text-md font-light">
-            <div className="hover:underline w-fit">
-            <Link
-                href={"#"}
-            >Ropa</Link>
-            </div>
-            <span>/</span>
-            <div className="hover:underline w-fit">
-            <Link href={"#"}>Hombre</Link>
-            </div>
-            <span>/</span>
-            <div className="hover:underline w-fit">
-            <Link href={"#"}>Chamarra</Link>
-            </div>
+            {
+                crumbs.categoria && (
+                    <div className="hover:underline w-fit">
+                        <Link href={"#"}>{crumbs.categoria}</Link>
+                    </div>
+                )
+            }
+
+            {
+                crumbs.subcategoria && (
+                    <>
+                        <span>/</span>
+                        <div className="hover:underline w-fit">
+                            <Link href={"#"}>{crumbs.subcategoria}</Link>
+                        </div>
+                    </>
+                )
+            }
+            {
+                crumbs.genero && (
+                    <>
+                        <span>/</span>
+                        <div className="hover:underline w-fit">
+                            <Link href={"#"}>{crumbs.genero}</Link>
+                        </div>
+                    </>
+                )
+            }
         </div>
     );
 }
