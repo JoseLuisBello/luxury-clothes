ffa77be08a870fa916865b9af4420bf243bb1eec
adrien04hz <adrien.ahs@gmail.com>
2026-04-15 17:06:25 -0600

    [FIX] Correción de escala y tamaño de imagenes

--- app/productos/components/GalleryDetails.tsx
+++ app/productos/components/GalleryDetails.tsx
@@ -15,30 +15,26 @@ export default function GalleryDetails({
             {/* Imagenes de costado */}
             <div className="flex flex-col justify-start items-center space-y-3">
                 {data.imagenes && data.imagenes.map((imgUrl, index) => (
-                    <div className="w-17.5 h-17.5 overflow-hidden rounded-[10px] flex justify-center items-center" key={index}>
+                    <div className="w-17.5 h-17.5 overflow-hidden rounded-[10px] relative border-gray-200 border" key={index}>
                         <Image
                             key={index}
                             src={imgUrl}
                             alt={`${data.nombre} - Imagen ${index + 1}`}
-                            width={70}
-                            height={100}
-                            className="mb-4 cursor-pointer rounded-[10px] hover:opacity-80"
+                            fill
+                            className="mb-4 object-cover cursor-pointer rounded-[10px] hover:opacity-80"
                             onClick={() => setURL(imgUrl)}
                             onMouseEnter={() => setURL(imgUrl)}
-                            objectFit="cover"
-                            loading="lazy"
                         />
                     </div>
                 ))}
             </div>
             {/* Imagen principal */}
-            <div className="w-133.75 h-167.25">
+            <div className="w-133.75 h-167.25 relative overflow-hidden">
                 <Image
                     src={url ? url.toString() : data.imagenes?.[0] || "/placeholder.png"}
                     alt={data.nombre}
-                    width={400}
-                    height={400}
-                    className="object-cover w-full h-full rounded-[10px] border border-gray-200"
+                    fill
+                    className="object-cover rounded-[10px] border border-gray-200"
                     loading="eager"
                 />
             </div>
