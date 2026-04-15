5221316833111c3b82037512d466cce463212a29
adrien04hz <adrien.ahs@gmail.com>
2026-04-15 04:17:24 -0600

    [UPDATE] Query para obtener categorias por genero

--- repositories/producto/categoria/categoria.repository.ts
+++ repositories/producto/categoria/categoria.repository.ts
@@ -11,29 +11,34 @@ export class CategoriaRepository {
      * @returns Lista de categorías con sus subcategorías
      */
 
-    static async getCategoriasConSubcategorias() {
+    static async getCategoriasConSubcategorias( id_genero: number ) {
         const { rows } = await pool.query(
             `
-                SELECT
-                    c.id AS id,
-                    c.nombre AS name,
-
-                    COALESCE(
-                    (
-                        SELECT json_agg(
-                        json_build_object(
-                            'id', s.id,
-                            'nombre', s.nombre
-                        )
-                        )
-                        FROM "Subcategoria" s
-                        WHERE s.id_categoria = c.id
-                    ),
-                    '[]'
-                    ) AS subcategories
-
-                FROM "Categoria" c;
-            `
+            SELECT 
+            c.id AS id_categoria,
+            c.nombre AS categoria,
+            json_agg(
+                jsonb_build_object(
+                'id', s.id,
+                'nombre', s.nombre,
+                'total', s.total_productos
+                )
+            ) AS subcategorias
+            FROM "Categoria" c
+            JOIN (
+            SELECT 
+                s.id,
+                s.nombre,
+                s.id_categoria,
+                COUNT(p.id) as total_productos
+            FROM "Subcategoria" s
+            JOIN "Producto" p ON p.id_subcategoria = s.id
+            WHERE p.id_genero = $1
+            GROUP BY s.id, s.nombre, s.id_categoria
+            ) s ON s.id_categoria = c.id
+            GROUP BY c.id, c.nombre
+            ORDER BY c.nombre;
+            `, [id_genero]
         );
         return rows;
     }
