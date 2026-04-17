813d4daef85d408809b8aeead9ff1c52b4c9e2fc
valm <valm040723@gs.utm.mx>
2026-04-17 10:35:54 -0600

    [DELETE] Archivo sin uso

--- client/direccionenvio.ts
+++
@@ -1,8 +0,0 @@
-import { apiFetch } from "@/lib/api";
-
-import { DireccionEnvio } from "@/types/direccionesenvio/DireccionesEnvio";
-
-//mostrar la lista de direcciones de envio disponnibles
-export const getTodasLasDirecciones = (id_cliente: number) : Promise<DireccionEnvio> => {
-    return apiFetch(`/direcciondeenvio?clientId=${id_cliente}`);
-}
