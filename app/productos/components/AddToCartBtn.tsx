4cba9fe99d5e73b2439e3ed01e9ce184bb4b72f1
adrien04hz <adrien.ahs@gmail.com>
2026-04-18 14:25:18 -0600

    [UPDATE] Boton carrito componente

--- app/productos/components/AddToCartBtn.tsx
+++ app/productos/components/AddToCartBtn.tsx
@@ -3,28 +3,61 @@
 
 import { useState } from "react";
 
-export default function AddToCartButton({ productoId, talla }) {
+export default function AddToCartButton({
+    id_producto,
+    id_Talla,
+    id_usuario,
+    onClick,
+} : {
+    id_producto: number;
+    id_Talla: number;
+    id_usuario: number;
+    onClick: (seleccina : boolean) => void;
+}) {
   const [loading, setLoading] = useState(false);
 
-  const handleClick = async () => {
-    if (!talla) {
-      alert("Selecciona una talla");
-      return;
-    }
+    const handleAddToCart = async () => {
+        setLoading(true);
+        if (!id_Talla) {
+            onClick(true);
+            setLoading(false);
+            return;
+        }
 
-    setLoading(true);
+        try {
+            const res = await fetch("/api/carrito", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    id_usuario: id_usuario,
+                    id_producto: id_producto,
+                    id_talla: id_Talla,
+                    cantidad: 1
+                }),
+            });
 
-    await fetch("/api/carrito", {
-      method: "POST",
-      body: JSON.stringify({ productoId, talla }),
-    });
+            if (res.status === 401) {
+                window.location.href = "/auth/login";
+                return;
+            }
 
-    setLoading(false);
-  };
+            if (!res.ok) {
+                throw new Error("Error al agregar al carrito");
+            }
+
+            console.log("Producto agregado");
+            setLoading(false);
+        } catch (error) {
+            console.error(error);
+            setLoading(false);
+        }
+    };
 
   return (
     <button
-      onClick={handleClick}
+      onClick={handleAddToCart}
       className="bg-black text-white py-2 px-4 rounded-[28px] w-full h-16 hover:opacity-60"
     >
       {loading ? "Agregando..." : "Agregar al carrito"}
