4d3a8964a6021b4dffd53f61be3cd8a05ed30430
adrien04hz <adrien.ahs@gmail.com>
2026-04-18 13:36:58 -0600

    [ADD] Dependencias de redux

--- app/carrito/page.tsx
+++ app/carrito/page.tsx
@@ -9,23 +9,57 @@ export default function CarritoPage() {
   const genero = 'Tenis para hombre';
 
   return (
-    <div className="w-full h-full p-24">
+    <div className="w-full h-full p-24 flex justify-center gap-4">
       {/* Contenedor para la bolsa */}
-      <div className="flex flex-col gap-3 items-center">
-        <ProductCardCart 
-          name={name}
-          price={price}
-          image={image}
-          color={color}
-          talla={talla}
-          genero={genero}
-          id={1}
-        />
+      <div className="flex flex-col gap-4 items-start w-fit">
+        <p className="text-3xl font-medium">Carrito de compra</p>
 
+        <div className="w-full">
+          <ProductCardCart
+            name={name}
+            price={price}
+            image={image}
+            color={color}
+            talla={talla}
+            genero={genero}
+            id={1}
+          />
+        </div>
       </div>
 
       {/* Contenedor para el resumen de compra */}
-      <div>
+      <div className="w-90 flex flex-col gap-4">
+        <p className="text-3xl font-medium">Resumen</p>
+
+        {/* gastos y subtotal */}
+        <div className="font-medium opacity-80 flex flex-col gap-4">
+          <div className="flex justify-between">
+            <p>Subtotal</p>
+            <p>${Number(price).toLocaleString()}</p>
+          </div>
+
+          <div className="flex justify-between">
+            <p>Gastos de envío estimados</p>
+            <p>Gratis</p>
+          </div>
+          <div className="border border-gray-200 w-full"></div>
+        </div>
+
+        {/* total */}
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-between font-semibold">
+            <p>Total</p>
+            <p>${Number(price).toLocaleString()}</p>
+          </div>
+          <div className="border border-gray-200 w-full"></div>
+        </div>
+
+        {/* Boton */}
+        <div className="w-full mt-4">
+          <button className="bg-black text-white py-2 px-4 rounded-full w-full h-16 hover:opacity-60 font-semibold">
+              Realizar compra
+          </button>
+        </div>
 
       </div>
     </div>
