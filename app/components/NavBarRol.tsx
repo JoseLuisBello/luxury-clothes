ecf7dc9e82d1f74f271e7d90e2f636a17866f304
valm <valm040723@gs.utm.mx>
2026-04-21 20:48:45 -0600

    [ADD] NavBarRol

--- app/components/NavBarRol.tsx
+++ app/components/NavBarRol.tsx
@@ -1,35 +1,25 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client";
 
-export default async function NavBarRol() {
+import { useEffect, useState } from "react";
+import NavBar from "./NavBar";
+import NavBarLogistica from "./NavBarLogistica";
 
+export default function NavBarRol(props: any) {
+  const [rol, setRol] = useState<number | null>(null);
 
-  return (
-    <>
-    <div className="w-full h-28 bg-black text-white flex justify-between px-12">
-      <Link href="/" className="flex items-center justify-center">
-        <Image 
-          src="/assets/logo/main-logo.svg" 
-          alt="Logo" 
-          loading="eager"
-          width={174} 
-          height={80} 
-        />
-      </Link>
-        
-      
-      <div className="flex space-x-7 items-center">
-        <div className="flex space-x-5">
-          <Link href="/logistica/enviopendiente">
-          <p>Envio</p>
-          </Link>
-          <Link href="/cuenta">
-            <Image src="/assets/images/profile.svg" alt="User" width={30} height={30} />
-          </Link>
-         
-        </div>
-      </div>
-    </div>
-    </>
-  );
+  useEffect(() => {
+    const rolStorage = localStorage.getItem("rol");
+    if (rolStorage) {
+      setRol(Number(rolStorage));
+    }
+  }, []);
+
+  // Loader opcional
+  if (rol === null) return null;
+
+  if (rol === 3 || rol === 4) {
+    return <NavBarLogistica />;
+  }
+
+  return <NavBar {...props} />;
 }
